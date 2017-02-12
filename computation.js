66d0fea14e1b3c099614c513a6c65d36c0c50800
jellyLeaf <lijinyi2016@sina.com>
2017-02-11 20:36:33 -0800

    calculation
    cal

--- computation.js
+++ computation.js
@@ -3,9 +3,45 @@ let compute = function() {
 	let f = document.expression;
 	let str = f.elements["expression"].value;
 
+	var a = new Array;
+	var b = 0;
+	function num(digit) {
+	    b = b * 10 + digit;
+	}
+	function operator(operator) {
+	    a.push(b);
+	    b = 0;
+	    a.push(operator);
+	}
+	function calculate() {
+	    for (var i = 0; i < a.length; i++) {
+	        if (a[i] == '*') {
+	            a[i] = a[i - 1] * a[i + 1];
+	            a.splice(i - 1, 1);
+	            a.splice(i + 1, 1);
+	        }
+	        else if (a[i] == '/') {
+	            a[i] = a[i - 1] / a[i + 1];
+	            a.splice(i - 1, 1);
+	            a.splice(i + 1, 1);
+	        }
+	    }
+	    for (var i = 0; i < a.length; i++) {
+	        if (a[i] == '+') {
+	            a[i] = a[i - 1] + a[i + 1];
+	            a.splice(i - 1, 1);
+	            a.splice(i + 1, 1);
+	        }
+	        else if (a[i] == '-') {
+	            a[i] = a[i - 1] - a[i + 1];
+	            a.splice(i - 1, 1);
+	            a.splice(i + 1, 1);
+	        }
 
+	    } 
+	}
 
-	let ans = str;
+	let ans = a[0].toString();
 	let answer = document.getElementById("answer");
 	answer.innerHTML = "";
 	answer.appendChild(document.createTextNode(ans));
