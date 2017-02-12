1b9ba8a5fce727cc640a905e8a84bc4d68815b47
justindli <justinli@s-169-232-87-99.resnet.ucla.edu>
2017-02-11 20:45:06 -0800

    Eval function

--- computation.js
+++ computation.js
@@ -3,45 +3,7 @@ let compute = function() {
 	let f = document.expression;
 	let str = f.elements["expression"].value;
 
-	var a = new Array;
-	var b = 0;
-	function num(digit) {
-	    b = b * 10 + digit;
-	}
-	function operator(operator) {
-	    a.push(b);
-	    b = 0;
-	    a.push(operator);
-	}
-	function calculate() {
-	    for (var i = 0; i < a.length; i++) {
-	        if (a[i] == '*') {
-	            a[i] = a[i - 1] * a[i + 1];
-	            a.splice(i - 1, 1);
-	            a.splice(i + 1, 1);
-	        }
-	        else if (a[i] == '/') {
-	            a[i] = a[i - 1] / a[i + 1];
-	            a.splice(i - 1, 1);
-	            a.splice(i + 1, 1);
-	        }
-	    }
-	    for (var i = 0; i < a.length; i++) {
-	        if (a[i] == '+') {
-	            a[i] = a[i - 1] + a[i + 1];
-	            a.splice(i - 1, 1);
-	            a.splice(i + 1, 1);
-	        }
-	        else if (a[i] == '-') {
-	            a[i] = a[i - 1] - a[i + 1];
-	            a.splice(i - 1, 1);
-	            a.splice(i + 1, 1);
-	        }
-
-	    } 
-	}
-
-	let ans = a[0].toString();
+	let ans = eval(str);
 	let answer = document.getElementById("answer");
 	answer.innerHTML = "";
 	answer.appendChild(document.createTextNode(ans));
